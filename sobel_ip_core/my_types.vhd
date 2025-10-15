391155c9906a9d0eadff942f2442cab593be3615
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2025-10-15 11:39:45 +0300

    Refactor my_types.vhd to replace 'digits' with specific constants for pixel, kernel, and gradient widths

--- sobel_ip_core/my_types.vhd
+++ sobel_ip_core/my_types.vhd
@@ -3,13 +3,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 package my_types is
-    constant digits : positive := 8;
+    constant pixel_width : positive := 8;
+    constant kernel_width : positive := pixel_width + 3;
+    constant gradient_width : positive := pixel_width * 2;
     
     type kernel_indexes_t is array(0 to 8) of integer;
-    type buffer_array is array(0 to 8) of std_logic_vector(digits - 1 downto 0);
-    type pixel_window is array(0 to 2, 0 to 2) of std_logic_vector(digits - 1 downto 0);
-    type kernel_window is array(0 to 1, 0 to 2) of std_logic_vector(digits + 2 downto 0);
-    type kernel_outputs is array(0 to 1, 0 to 5) of std_logic_vector((digits + 2) downto 0);
-    type gradient_pair is array(0 to 1) of std_logic_vector((digits + 8) - 1 downto 0);
+    type buffer_array is array(0 to 8) of std_logic_vector(pixel_width - 1 downto 0);
+    type pixel_window is array(0 to 2, 0 to 2) of std_logic_vector(pixel_width - 1 downto 0);
+    type kernel_window is array(0 to 1, 0 to 2) of std_logic_vector(kernel_width - 1 downto 0);
+    type kernel_outputs is array(0 to 1, 0 to 5) of std_logic_vector(kernel_width - 1 downto 0);
+    type gradient_pair is array(0 to 1) of std_logic_vector(gradient_width - 1 downto 0);
     
 end package my_types;
