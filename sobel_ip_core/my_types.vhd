2d1d6b94d22984e7ebc43fd767950cb54109ae3c
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2025-10-18 14:07:20 +0300

    Refactor my_types.vhd to improve type definitions and clarify pixel and gradient constants

--- sobel_ip_core/my_types.vhd
+++ sobel_ip_core/my_types.vhd
@@ -1,17 +1,14 @@
--- Custom types package for Sobel edge detector
+-- Definition of pixel and gradient types used in Sobel IP core
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 package my_types is
-    constant pixel_width : positive := 8;
-    constant kernel_width : positive := pixel_width + 3;
-    constant gradient_width : positive := pixel_width * 2;
+
+    constant pixel_width : positive := 8; -- 8-bit pixel values for grayscale images [0-255]
+    constant gradient_width : positive := 12; -- 12-bit gradients to accommodate larger range after convolution + manhattan norm 
     
-    type kernel_indexes_t is array(0 to 8) of integer;
-    type buffer_array is array(0 to 8) of std_logic_vector(pixel_width - 1 downto 0);
-    type pixel_window is array(0 to 2, 0 to 2) of std_logic_vector(pixel_width - 1 downto 0);
-    type kernel_window is array(0 to 1, 0 to 2) of std_logic_vector(kernel_width - 1 downto 0);
-    type kernel_outputs is array(0 to 1, 0 to 5) of std_logic_vector(kernel_width - 1 downto 0);
-    type gradient_pair is array(0 to 1) of std_logic_vector(gradient_width - 1 downto 0);
-    
+    type pixel_window is array(0 to 2, 0 to 2) of std_logic_vector(pixel_width - 1 downto 0); -- 3x3 pixel window outputted by window buffer
+    type gradient_pair is array(0 to 1) of std_logic_vector(gradient_width - 1 downto 0); -- Gradient pair outputted by kernel application (0: Gx, 1: Gy)
+
 end package my_types;
