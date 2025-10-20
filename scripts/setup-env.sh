e2b760046aeab13605756b7eb037a8dabb720640
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2025-10-20 15:25:03 +0300

    Update setup-env.sh for Ubuntu 22.04 compatibility
    - Replace zlib1g:i386 with lib32z1-dev to fix PetaLinux installer zlib error
    - Update outdated package names (e.g., libncurses5-dev -> libncurses-dev, tftpd -> tftpd-hpa, libsdl1.2-dev -> libsdl2-dev)
    - Add multi-arch setup (i386) and apt update steps
    - Include note specifying the script is for Ubuntu 22.04

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -2,10 +2,11 @@
 # environment in order to install and use the PetaLinux tools.
 # It installs a series of libraries and applications required
 # by PetaLinux tools.
+# Note: This script is tailored for Ubuntu 22.04. For other versions, check PetaLinux documentation.
 # Creator : marrln
 # Date    : October 20, 2025
 # Last Modification : October 20, 2025
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt-get install -y gcc git make net-tools libncurses-dev tftpd-hpa zlib1g-dev libssl-dev flex bison libselinux1 gnupg wget diffstat chrpath socat xterm autoconf libtool tar unzip texinfo gcc-multilib build-essential libsdl2-dev libglib2.0-dev zlib1g:i386 screen pax gzip gawk
+sudo apt-get install -y gcc git make net-tools libncurses-dev tftpd-hpa zlib1g-dev libssl-dev flex bison libselinux1 gnupg wget diffstat chrpath socat xterm autoconf libtool tar unzip texinfo gcc-multilib build-essential libsdl2-dev libglib2.0-dev lib32z1-dev screen pax gzip gawk
